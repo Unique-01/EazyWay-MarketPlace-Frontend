f3b2c0b9fffe0981f27b37cb73b04bb11b60551f
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2024-08-15 16:15:03 +0100

    Complete main footer design by adding contact and payments logo

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom";
 import Subscribe from "./FooterNewsletter";
 import AppLogo from "assets/images/eazyWay-logo.png";
 import "./Footer.css";
+import ApplePay from "assets/images/ApplePay.svg";
+import Discover from "assets/images/Discover.svg";
+import Mastercard from "assets/images/Mastercard.svg";
+import SecurePayment from "assets/images/SecurePayment.svg";
+import Visa from "assets/images/Visa.svg";
 
 const Footer = () => {
     return (
@@ -93,6 +98,49 @@ const Footer = () => {
                                 </nav>
                             </div>
                         </div>
+                        <div className="mt-4 mt-lg-0 footer-contact">
+                            <a
+                                className="text-white border-bottom border-primary pb-2"
+                                href="tel:+234 779 8499 895">
+                                +234 779 8499 895
+                            </a>
+                            <span className="px-3 text-secondary">or</span>
+                            <a
+                                href="mailto:hello@gmail.com"
+                                className="text-white border-bottom border-primary pb-2">
+                                hello@gmail.com
+                            </a>
+                        </div>
+                    </div>
+                    <div className="pt-4">
+                        <hr className="text-white" />
+                    </div>
+                    <div className="d-lg-flex justify-content-between align-items-center">
+                        <p className="text-secondary d-none d-lg-block">
+                            D-EazyWay Marketplace &copy; 2024 All Rights
+                            Reserved
+                        </p>
+                        <div className="footer-payment d-flex gap-2 gap-md-3 mt-4 mt-lg-0">
+                            <div>
+                                <img src={ApplePay} alt="Apple Pay" className=""/>
+                            </div>
+                            <div>
+                                <img src={Visa} alt="Visa" className="" />
+                            </div>
+                            <div>
+                                <img src={Discover} alt="Discover" className="" />
+                            </div>
+                            <div>
+                                <img src={Mastercard} alt="Master card" className="" />
+                            </div>
+                            <div>
+                                <img src={SecurePayment} alt="Secure Payment" className="secure-payment" />
+                            </div>
+                        </div>
+                        <p className="text-secondary d-lg-none mt-3">
+                            D-EazyWay Marketplace &copy; 2024 All Rights
+                            Reserved
+                        </p>
                     </div>
                 </div>
             </footer>
