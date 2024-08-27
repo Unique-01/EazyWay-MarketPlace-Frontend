3a8878b23c2942de6687e2d888fe676a8caebed3
Abdulazeez Saheed Olawale <86983696+Unique-01@users.noreply.github.com>
2024-08-27 11:10:31 +0100

    Feature/product list (#8)
    * Complete product List page
    
    * Update product card to set link to detail on image and text only, not the whole product card component

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,10 +1,11 @@
-const Button = ({ text, handleClick }) => {
+const Button = ({ text, icon, handleClick }) => {
     return (
         <div>
             <button
                 onClick={handleClick}
                 className="btn btn-primary rounded-pill text-white px-4 py-2 fw-medium">
                 {text}
+                {icon && <span className="ms-2">{icon}</span>}
             </button>
         </div>
     );
