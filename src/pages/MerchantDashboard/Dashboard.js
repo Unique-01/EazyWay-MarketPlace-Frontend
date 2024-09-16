30f125eb55cdd026d01e14f0082f7fcf8afab425
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2024-09-16 13:01:43 +0100

    add Top selling product table

--- src/pages/MerchantDashboard/Dashboard.js
+++ src/pages/MerchantDashboard/Dashboard.js
@@ -1,15 +1,25 @@
+import TopSellingProduct from "./components/TopSellingProduct";
 import Totals from "./components/Totals";
+import products from "pages/Products/products.json";
 
 const MerchantDashboard = () => {
     return (
         <div className="py-5">
-            <div style={{minHeight:"100vh"}}> 
+            <div style={{ minHeight: "100vh" }}>
                 <Totals
                     sales={"10,550"}
                     orders={"1,200"}
                     products={700}
                     customers={500}
                 />
+                <div className="row mt-4">
+                    <div className="col-lg-8">
+                        <TopSellingProduct
+                            productList={products}
+                            itemsPerPage={5}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
